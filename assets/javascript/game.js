8d3c9f50a70f6dc64be535d9e57b8957509e7d91
majedline <majed.atwi@gmail.com>
2019-06-08 15:42:46 -0400

    Added keyPressed, isCorrectGuess, loadInnerGamecontent to game. Need to fix loadInnerGameContent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,44 @@
 
+var gameLimit = 10;
+
 var petList = ["cat", "dog", "mouse", "parrot", "rabbit", "lizard"];
 
 var currentGame = {
-    petToGuess : "",
-    numberOfGuessesMade: 0, 
-    numberOfGuessesRemaining: 10,
+    petToGuess: "",
+    numberOfGuessesMade: 0,
+    numberOfGuessesRemaining: gameLimit,
     charactersToGuess: [],
+    correctCharactersGuessedByUser: [],
+    wrongCharactersGuessedByUser: [],
 };
 
-function setGame(){
+function setGame() {
 
     currentGame.numberOfGuessesMade = 0;
-    currentGame.numberOfGuessesRemaining = 10
+    currentGame.numberOfGuessesRemaining = gameLimit;
 
     // get the random workd    
     currentGame.petToGuess = petList[Math.floor(Math.random() * petList.length)];
     console.log(currentGame.petToGuess);
 
-    // split the word
+    // split the word, and set the game value
     currentGame.charactersToGuess = currentGame.petToGuess.split("");
+    currentGame.correctCharactersGuessedByUser.length = currentGame.charactersToGuess.length;
+
     console.log(currentGame.charactersToGuess);
-   
+    console.log(currentGame.correctCharactersGuessedByUser);
+    console.log(currentGame.wrongCharactersGuessedByUser);
+
+
     //for every spot in chracters to guess array, place a div
     var mainDiv = document.createElement("div");
     mainDiv.id = "char-to-guess-area";
     mainDiv.className = "row";
-    
-    for (var i=0; i<currentGame.charactersToGuess.length; i++){
+
+    for (var i = 0; i < currentGame.charactersToGuess.length; i++) {
         var innerDiv = document.createElement("div");
         innerDiv.className = "col-sm char-to-guess";
-        innerDiv.id = "char-to-guess-"+i;
+        innerDiv.id = "char-to-guess-" + i;
         innerDiv.innerHTML = "_";
 
         mainDiv.append(innerDiv);
@@ -40,12 +49,80 @@ function setGame(){
     //document.getElementById("demo").innerHTML = "The Unicode CHARACTER code is: " + char;
 }
 
-function reset(){
-    document.getElementById("game-area").innerHTML="";
+function reset() {
+    document.getElementById("game-area").innerHTML = "";
     setGame();
 }
 
+// need to load the array into the game area //// TO REVIEW
+function loadInnerGame() {
+    //for every spot in chracters to guess array, place a div
+    var mainDiv = document.createElement("div");
+    mainDiv.id = "char-to-guess-area";
+    mainDiv.className = "row";
+
+    for (var i = 0; i < currentGame.correctCharactersGuessedByUser.length; i++) {
+        var innerDiv = document.createElement("div");
+        innerDiv.className = "col-sm char-to-guess";
+        innerDiv.id = "char-to-guess-" + i;
+
+        // load the correct characters
+        if (correctCharactersGuessedByUser[i] != null) {
+            innerDiv.innerHTML = currentGame.correctCharactersGuessedByUser[i];
+
+        } else {
+            innerDiv.innerHTML = "_";
+        }
+
+        mainDiv.append(innerDiv);
+    }
+    console.log(mainDiv);
+    document.getElementById("game-area").append(mainDiv);
+
+}
+
+// When a key is pressed, check if it is a correct guess
+function keyPressed() {
+    document.onkeyup = function (event) {
+        alert(event.key);
+        isCorrectGuess(event.key)
+    };
+}
+
+// if the guess charcter is found, then put it in the correct bucket. If not, then put in the worng bucket
+function isCorrectGuess(guessedCharacter) {
+
+    // based on the charcter entered, go throuh the list of charcaters to guess. If any of them match, then add them to the correct guesses
+    var foundCorrectGuess = false;
+
+    for (var i = 0; i < currentGame.charactersToGuess.length; i++) {
+        if (guessedCharacter === currentGame.charactersToGuess[i]) {
+            currentGame.correctCharactersGuessedByUser[i] = currentGame.charactersToGuess[i];
+            foundCorrectGuess = true;
+        }
+    }
+
+
+    // increment number of guesses made
+    currentGame.numberOfGuessesMade++;
+
+    //decrment number of guesses remaining
+    currentGame.numberOfGuessesRemaining--;
+
+    if (!foundCorrectGuess) {
+        currentGame.wrongCharactersGuessedByUser.push(guessedCharacter);
+    }
+
+    console.log(currentGame.charactersToGuess);
+    console.log(currentGame.correctCharactersGuessedByUser);
+    console.log(currentGame.wrongCharactersGuessedByUser);
+
+
+
+}
+
 setGame();
+keyPressed();
 
 // var car = {
 //     make: "Honda",
